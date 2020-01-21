0c8680b3771592d3d3aad644cd3de108ba7350d7
root <georgekimuu@gmail.com>
2020-01-21 15:27:47 +0300

    Generating fake Answers

--- app/Answer.php
+++ app/Answer.php
@@ -8,7 +8,7 @@ class Answer extends Model
 {
     public function question()
     {
-        return $this->belongTo(Question::class);
+        return $this->belongsTo(Question::class);
     }
 
     public function answer()
@@ -19,5 +19,17 @@ class Answer extends Model
     public function getBodyHtmlAttribute()
     {
         return \Parsedown::instance()->text($this->body);
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::created(function($answer){
+            $answer->question->increment('answers_count');
+        });
+
+
+
     }
 }
