ffc15af938d150fefc7e12cfde9ae92e43590882
root <georgekimuu@gmail.com>
2020-01-21 15:27:47 +0300

    Generating fake Answers

--- app/Answer.php
+++ app/Answer.php
@@ -6,9 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Answer extends Model
 {
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
     public function question()
     {
-        return $this->belongTo(Question::class);
+        return $this->belongsTo(Question::class);
     }
 
     public function answer()
@@ -19,5 +24,19 @@ class Answer extends Model
     public function getBodyHtmlAttribute()
     {
         return \Parsedown::instance()->text($this->body);
+    }
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::created(function($answer){
+            $answer->question->increment('answers_count');
+        });
+    }
+    public function getCreatedDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
+
     }
 }
